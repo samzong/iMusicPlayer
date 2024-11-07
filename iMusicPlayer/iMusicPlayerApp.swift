d0a3107a3bb72537e969f73d56e9f02f6c68b10d
samzong <samzong.lu@gmail.com>
2024-11-07 08:21:15 +0800

    Initial Commit

---
+++ iMusicPlayer/iMusicPlayerApp.swift
@@ -0,0 +1,17 @@
+//
+//  iMusicPlayerApp.swift
+//  iMusicPlayer
+//
+//  Created by X on 11/7/24.
+//
+
+import SwiftUI
+
+@main
+struct iMusicPlayerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
